8a80b5f42fa02ce1cd5d6137328daea2dc784706
Evan Alto <evanalto@Evans-MacBook.local>
2019-05-04 16:31:44 -0500

    Updates

---
+++ precache-manifest.4691cbceafc4b5f3cf5bd49bac5140e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a111daa2f0bb372e2da8cd1df89fd062",
+    "url": "/daily-app-5-4-19/index.html"
+  },
+  {
+    "revision": "87072033eee381f0c884",
+    "url": "/daily-app-5-4-19/static/css/main.ef22cd4b.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/daily-app-5-4-19/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "87072033eee381f0c884",
+    "url": "/daily-app-5-4-19/static/js/main.07d86fc2.chunk.js"
+  },
+  {
+    "revision": "e35a57b7529d85f0fdf4",
+    "url": "/daily-app-5-4-19/static/js/runtime~main.0d6ed40b.js"
+  }
+]);
